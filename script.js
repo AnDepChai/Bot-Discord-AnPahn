d2ab335cf483f213cf1e695d6a537a9ff95f755a
An Pahn <128393021+AnDepChai@users.noreply.github.com>
2024-09-17 23:08:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+// Animation for features section
+gsap.from(".section-title", { duration: 1, y: 50, opacity: 0, ease: "power2.out", delay: 0.5 });
+gsap.from(".card", {
+    duration: 1,
+    opacity: 0,
+    scale: 0.9,
+    stagger: 0.2,
+    ease: "power2.out",
+    delay: 1
+});
